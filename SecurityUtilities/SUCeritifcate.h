eb887b83b79259a63804f905f1153d2c4f440dbf
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-12 09:12:08 +0300

    Added cerificate adapter empty class.

---
+++ SecurityUtilities/SUCeritifcate.h
@@ -0,0 +1,16 @@
+//
+//  SUCeritifcate.h
+//  certificateUtility
+//
+//  Created by Danil Korotenko on 8/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SUCeritifcate : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SecurityUtilities/SUCeritifcate.m
@@ -0,0 +1,12 @@
+//
+//  SUCeritifcate.m
+//  certificateUtility
+//
+//  Created by Danil Korotenko on 8/12/24.
+//
+
+#import "SUCeritifcate.h"
+
+@implementation SUCeritifcate
+
+@end
