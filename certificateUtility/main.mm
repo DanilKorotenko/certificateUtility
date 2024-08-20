4a3d714eda30611366257dc167673147390f7d96
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-20 08:33:14 +0300

    Moved everything to main.

--- certificateUtility/main.mm
+++ certificateUtility/main.mm
@@ -8,12 +8,30 @@
 #import <Foundation/Foundation.h>
 #import "../SecurityUtilities/SUCeritifcate.h"
 #import "../SecurityUtilities/SUKeychain.h"
-#import "../AuthorizationUtilities/AUAuthorization.h"
 #import "../IdentityUtilities/IUIdentity.h"
 #import "../IdentityUtilities/IUIdentityQuery.h"
 
 const char *userLogin = "trustadmin";
 const char *userPass = "pass123456";
+
+AuthorizationEnvironment createEnvironment(const char *aLogin, const char *aPassword)
+{
+    static AuthorizationItem authenv[] =
+    {
+        { kAuthorizationEnvironmentUsername },
+        { kAuthorizationEnvironmentPassword },
+        { kAuthorizationEnvironmentShared }
+    };
+
+    AuthorizationEnvironment env = { 0, authenv };
+    authenv[0].valueLength = strlen(aLogin);
+    authenv[0].value = (void *)aLogin;
+    authenv[1].valueLength = strlen(aPassword);
+    authenv[1].value = (void *)aPassword;
+    env.count = 3;
+
+    return env;
+}
 
 int main(int argc, const char * argv[])
 {
@@ -57,35 +75,63 @@ int main(int argc, const char * argv[])
             return 0;
         }
 
-        OSStatus myStatus = executeTrustSettingsAdminAuthorizedBlock(userLogin, userPass,
-            ^{
-                OSStatus err = noErr;
-                SUKeychain *keychain = [[SUKeychain alloc] initSystemKeychain];
-                if (!keychain)
-                {
-                    NSLog(@"No keychain");
-                    return;
-                }
+        __block OSStatus err = noErr;
+        SUKeychain *keychain = [[SUKeychain alloc] initSystemKeychain];
+        if (!keychain)
+        {
+            NSLog(@"No keychain");
+            return 0;
+        }
 
-                if (![keychain containsCertificate:certificate])
-                {
-                    err = [keychain addCertificate:certificate];
-                    if (err != noErr)
-                    {
-                        NSLog(@"SecCertificateAddToKeychain failure. Error: %d", err);
-                    }
-                }
-                else
-                {
-                    NSLog(@"Certificate already in keychain");
-                }
+        if (![keychain containsCertificate:certificate])
+        {
+            err = [keychain addCertificate:certificate];
+            if (err != noErr)
+            {
+                NSLog(@"SecCertificateAddToKeychain failure. Error: %d", err);
+            }
+        }
+        else
+        {
+            NSLog(@"Certificate already in keychain");
+        }
 
-                err = [certificate installAdminTrustSettings];
-                if (err != noErr)
-                {
-                    NSLog(@"SecTrustSettingsSetTrustSettings failure. Error: %d", err);
-                }
-            });
+        AuthorizationRef myAuthorizationRef = NULL;
+        OSStatus myStatus = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment,
+            kAuthorizationFlagDefaults, &myAuthorizationRef);
+
+        AuthorizationItem myItems[1];
+
+        myItems[0].name = "com.apple.trust-settings.admin";
+        myItems[0].valueLength = 0;
+        myItems[0].value = NULL;
+        myItems[0].flags = 0;
+
+        AuthorizationRights myRights;
+        myRights.count = sizeof (myItems) / sizeof (myItems[0]);
+        myRights.items = myItems;
+
+        AuthorizationFlags myFlags = kAuthorizationFlagDefaults | kAuthorizationFlagExtendRights;
+
+        AuthorizationEnvironment env = createEnvironment(userLogin, userPass);
+
+        AuthorizationRights *myAuthorizedRights = NULL;
+        myStatus = AuthorizationCopyRights(myAuthorizationRef, &myRights,
+            &env, myFlags, &myAuthorizedRights);
+
+        err = SecTrustSettingsSetTrustSettings(certificate.certificateRef, kSecTrustSettingsDomainAdmin, NULL);
+
+        if (err != noErr)
+        {
+            NSLog(@"SecTrustSettingsSetTrustSettings failure. Error: %d", err);
+        }
+
+        if (myAuthorizedRights)
+        {
+            AuthorizationFreeItemSet (myAuthorizedRights);
+        }
+
+        AuthorizationFree (myAuthorizationRef, kAuthorizationFlagDestroyRights);
 
         if (myStatus != noErr)
         {
