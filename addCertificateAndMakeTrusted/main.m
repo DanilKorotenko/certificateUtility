a488cbaccf22ad13004f885466bebc1aa4aa8291
Danil Korotenko <danil.korotenko@gmail.com>
2024-08-20 08:35:32 +0300

    New target added.

---
+++ addCertificateAndMakeTrusted/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  addCertificateAndMakeTrusted
+//
+//  Created by Danil Korotenko on 8/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
