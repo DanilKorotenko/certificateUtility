8ecc31db18006de67b199c9c4c6f96b0eb161a75
Danil Korotenko <danil.korotenko@gmail.com>
2024-09-18 13:26:30 +0300

    Added new target.

---
+++ certificateUtilityApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  certificateUtilityApp
+//
+//  Created by Danil Korotenko on 9/18/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
